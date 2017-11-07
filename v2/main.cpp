13a083c8073ebc111cb338e89ac978370df6ee99
catsymptote <catsymptote@gmail.com>
2017-11-07 20:11:09 +0100

    Objects in main initialized on the heap instead of the stack.

--- v2/main.cpp
+++ v2/main.cpp
@@ -7,48 +7,61 @@
 matrix matrixGen(int m, int n);
 int main()
 {
-    /// Create the matrix objects
-    matrix A;
-    matrix B;
-    matrix C;
-    matrix D;
+    /// Create the matrix pointers.
+    matrix *A;
+    matrix *B;
+    matrix *C;
+    matrix *D;
+    //matrix D;
+
+    /// Create the objects pointed to on the heap.
+    A = new matrix;
+    B = new matrix;
+    C = new matrix;
+    D = new matrix;
     //std::auto_ptr<matrix> A(new matrix);
 
     /// Get data from user.
-    A.getData();
-    B.getData();
+    A->getData();
+    B->getData();
 
 
     /// Do maths using the user input.
-    C = A * B;
-    D = A + B;
+    *C = *A * *B;
+    *D = *A + *B;
 
     /// Show and print to file.
-    A.show("A");
-    B.show("B");
-    C.show("AB");
-    D.show("A+B");
+    A->show("A");
+    B->show("B");
+    C->show("AB");
+    D->show("A+B");
 
     /// Print to file formatted for Wolfram Alpha.
-    A.printFileWA("A");
-    B.printFileWA("B");
-    C.printFileWA("AB");
-    D.printFileWA("A+B");
+    A->printFileWA("A");
+    B->printFileWA("B");
+    C->printFileWA("AB");
+    D->printFileWA("A+B");
 
 
-    /// Automatically generated matrices.
-    A = matrixGen(8, 8);
-    B = matrixGen(8, 8);
-    C = A * B;
-    D = A + B;
-    A.show("X");
-    B.show("Y");
-    C.show("XY");
-    D.show("X+Y");
-    A.printFileWA("X");
-    B.printFileWA("Y");
-    C.printFileWA("XY");
-    D.printFileWA("X+Y");
+    /// Example with automatically generated matrices.
+    *A = matrixGen(8, 8);
+    *B = matrixGen(8, 8);
+    *C = *A * *B;
+    *D = *A + *B;
+    A->show("X");
+    B->show("Y");
+    C->show("XY");
+    D->show("X+Y");
+    A->printFileWA("X");
+    B->printFileWA("Y");
+    C->printFileWA("XY");
+    D->printFileWA("X+Y");
+
+    /// Delete (run destructor) of objects on the heap.
+    delete A;
+    delete B;
+    delete C;
+    delete D;
 
     return 0;
 }
